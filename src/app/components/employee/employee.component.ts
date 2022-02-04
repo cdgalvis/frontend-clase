f1fe536815d9b9549c545504e3f0edad44239b8d
cdgalvis <cristiangalviscardona@gmail.com>
2022-02-04 18:37:17 -0500

    Modificacion add

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-employee',
@@ -21,6 +23,10 @@ export class EmployeeComponent implements OnInit {
       },
       err => console.log(err)
     );
+  }
+
+  addEmployee(form: NgForm){
+
   }
 
 }
