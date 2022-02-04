a8c0863a66b923bb33c9eee468f5189b747b21fd
cdgalvis <cristiangalviscardona@gmail.com>
2022-02-04 18:31:49 -0500

    Iniciamos addEmployee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './components/employee/employee.component';
@@ -12,7 +13,8 @@ import { EmployeeComponent } from './components/employee/employee.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
